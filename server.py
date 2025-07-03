a609d7391de1e070e70e2bdc34c3a0c1970537fa
Tyffoni <tyffoni@gmail.com>
2025-07-02 17:43:04 -0700

    render html test

--- server.py
+++ server.py
@@ -6,14 +6,14 @@ import random
 import subprocess
 import threading
 import time
+
 from subprocess import Popen, PIPE, STDOUT
 
 from sign_message import SignMessage
 
-initial = {"scrollSpeed": 5, "backgroundColor": "#ffffff", "textColor": "#000000", "borderColor": "#ffffff", "text": " "}
-
 proc = None
-sign_message = SignMessage(initial)
+sign_message = None
+
 app = Flask(__name__)
 parser = argparse.ArgumentParser()
 parser.add_argument(
@@ -99,7 +99,7 @@ def turn_off():
     success = False
     if args.development:
       success = True
-      sign_message = SignMessage(initial)
+      sign_message = None
 
     if proc != None:
         proc.kill()
@@ -137,8 +137,9 @@ def update_sign():
         sign_message = None
         return "Could not update sign", 500
 @app.route('/')
-def home():
-    return render_template('index.html',sign_message = sign_message)
+def home():       
+   return render_template('index.html')
+
 
 if __name__ == "__main__":
     # give the last opened an initial value of now,
